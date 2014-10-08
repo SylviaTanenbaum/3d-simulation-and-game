0c20eee8ea5c68f0a05af4b0a9e34a6003d9211f
xuduo <realmany@163.com>
2014-10-08 22:36:19 +0800

    add datas

--- Bin/Data/scripts/onCreateProject.lua
+++ Bin/Data/scripts/onCreateProject.lua
@@ -16,3 +16,17 @@ PX2_SS:SetPlaySameTimeRange(0.2)
 PX2_PROJ:SetShowProjectInfo(true)
 
 -- 游戏		-------------------------------------------------------------
+
+function OnBut ()
+	local scene = PX2_PROJ:GetScene()
+	local actor = scene:GetActor("Man")
+	actor:RegistToScriptSystem()
+	
+	local butName = this:GetName()
+	
+	if arg1 == UICT_PRESSED then		
+	elseif arg1 == UICT_RELEASED then		
+		actor:PlayAnimByName(butName)
+	elseif arg1 == UICT_RELEASED_NOTVALIED then		
+	end
+end
